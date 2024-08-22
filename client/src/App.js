0b5475e3132eaa2345c244c482f2e45232228bdd
RishabSharma2003 <141629276+RishabSharma2003@users.noreply.github.com>
2024-08-22 16:42:02 +0530

    client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+//from auth
+import Login from './components/auth/Login';
+
+
+function App() {
+  return (
+    <div>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
