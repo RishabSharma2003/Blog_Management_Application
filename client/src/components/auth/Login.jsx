1f1d0582225c989d169194bd56a2e9783c38f57e
RishabSharma2003 <141629276+RishabSharma2003@users.noreply.github.com>
2024-08-22 17:12:47 +0530

    c

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,27 +1,47 @@
 import React from 'react'
 import {Box, TextField, Button, styled} from '@mui/material'
 
-
-const Component=styled(Box)`
-    width:400px;
-    margin:auto;
-    box-shadow:5px 2px 5px 2px rgba(0 0 0/0.6);
-`
-
-const Image=styled('img')({
-    width:100
-})
-
 const Login = () => {
   return (
     <Component>
+      <Box>
         <Image src="https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png" alt="" />
-        <TextField id="standard-basic" label="Email" variant="standard" />
-        <TextField id="standard-basic" label="Password" variant="standard" />
-        <Button variant="contained">Login</Button>
-        <Button variant="Outlined">Create an account</Button>
+        <Wrappper>
+          <TextField id="standard-basic" label="Email" variant="standard" />
+          <TextField id="standard-basic" label="Password" variant="standard" />
+          <Button variant="contained">Login</Button>
+          <Button variant="Outlined">Create an account</Button>
+        </Wrappper>
+      </Box>
+
     </Component>
   )
 }
 
 export default Login
+
+
+
+//for image
+const Image=styled('img')({
+  width:100,
+  margin:'auto',
+  display:'flex',
+  padding:'50px 0 0'
+})
+//for outer layout
+const Component=styled(Box)`
+  width:400px;
+  margin:auto;
+  box-shadow:5px 2px 5px 2px rgba(0 0 0/0.6);
+`// for image and textField layout
+const Wrappper=styled(Box)`
+  padding:25px 35px;
+  display:flex;
+  flex-direction:column;
+  flex:1;
+  &>div,&button{
+    margin-top:20px
+  }
+`
+
